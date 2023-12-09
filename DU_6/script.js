0904051868b8856cca383816ea073004b122665f
daryazhur <130771599+daryazhur@users.noreply.github.com>
2023-12-09 12:06:54 +0100

    Add files via upload

--- DU_6/script.js
+++ DU_6/script.js
@@ -10,7 +10,7 @@ document.body.innerHTML += fillcut('petr', 3) + '<br>'; // vypíše „pet“
 document.body.innerHTML += fillcut('petr', 4) + '<br>'; // vypíše „petr“
 */
 
-fillcut = (str, len) => {
+const fillcut = (str, len) => {
     if(str.length === len) {
         return str
     } else if(str.length < len) {
@@ -18,7 +18,7 @@ fillcut = (str, len) => {
         const tecka = ".".repeat(pridatTecku)
         return tecka + str
     } else {
-        return str.slice(-len)
+        return str.slice(0, len)
     }
 }
 
